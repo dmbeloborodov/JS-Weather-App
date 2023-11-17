3f31f48d26e707a46eb22b9af4c0392895bfdbe6
Dmitrii Beloborodov <dmibel95@gmail.com>
2023-11-17 13:44:53 +0800

    connected the openweathermap api and add geolocation search by city name

--- js/mian.js
+++ js/mian.js
@@ -1,15 +1,25 @@
+const API_KEY = "ca4be9013bb6f2f5d76e0bfd85f072c5";
+
 const form = document.querySelector("#form");
 const input = document.querySelector(".form__input");
 
 form.onsubmit = submitHeander;
 
-function submitHeander(e) {
+async function submitHeander(e) {
   e.preventDefault();
 
-  if (!input.value) {
+  if (!input.value.trim()) {
     console.log("Enter city name please");
     return;
   }
 
-  console.log(input.value);
+  const cityInfo = await getGeo(input.value.trim());
+}
+
+async function getGeo(name) {
+  const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=5&appid=${API_KEY}`;
+  const response = await fetch(geoUrl);
+  const data = await response.json();
+  console.log(data);
+  return data;
 }
