b7f59c10fe473a3d697c774a18c0a44aa381ba22
Dmitrii Beloborodov <dmibel95@gmail.com>
2023-11-17 14:26:14 +0800

    add getting weather by coordinates

--- js/mian.js
+++ js/mian.js
@@ -14,12 +14,21 @@ async function submitHeander(e) {
   }
 
   const cityInfo = await getGeo(input.value.trim());
+
+	const weatherInfo = await getWeather(cityInfo[0]["lat"], cityInfo[0]["lon"])
+	console.log(weatherInfo)
 }
 
 async function getGeo(name) {
   const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=5&appid=${API_KEY}`;
   const response = await fetch(geoUrl);
   const data = await response.json();
-  console.log(data);
+  return data;
+}
+
+async function getWeather(lat, lon) {
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  const response = await fetch(weatherUrl);
+  const data = await response.json();
   return data;
 }
