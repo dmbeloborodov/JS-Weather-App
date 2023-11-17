80779bbd5bfb6523ce208c2e483a1441bdcd805f
Dmitrii Beloborodov <dmibel95@gmail.com>
2023-11-17 13:11:10 +0800

    add a form submission handler

--- js/mian.js
+++ js/mian.js
@@ -1,1 +1,15 @@
-console.log('message')
+const form = document.querySelector("#form");
+const input = document.querySelector(".form__input");
+
+form.onsubmit = submitHeander;
+
+function submitHeander(e) {
+  e.preventDefault();
+
+  if (!input.value) {
+    console.log("Enter city name please");
+    return;
+  }
+
+  console.log(input.value);
+}
